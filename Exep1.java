eb24676a3f4724121bd825deafb509e5c16ac236
Ketan narhe <ketannarhe02@gmail.com>
2025-07-22 20:30:22 -0700

    Add files via upload

---
+++ Exep1.java
@@ -0,0 +1,16 @@
+package com.Exception;
+
+public class Exep1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		for(int i=1;i<=10;i++)
+		{                          
+			Thread.sleep(2000);                
+			System.out.println("Hii ketan");                        
+		}
+                                                    
+	}
+
+}
